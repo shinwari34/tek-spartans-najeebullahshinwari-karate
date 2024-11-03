c50dfaf9e64e025d189d67a84fdef116a342fe9a
Najeebullah <Najeebullah.shinwari34@gmail.com>
2024-11-03 01:23:25 -0400

    pass the commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,8 @@ public class TestRunner {
     Karate runTest() {
         return Karate
                 .run("classpath:features")
-                .karateEnv('dev')
-                .tags("@End2EndAccount");
+                .karateEnv("dev")
+                .tags("@Regression")
+                .outputCucumberJson(true);
     }
 }
